6a598978c699efb96f6eac3e010e54f6609665d6
skuminok <154214571+skuminok@users.noreply.github.com>
2024-01-10 03:18:17 +0500

    add Lesson_3:Task_3

---
+++ Lesson_2/Task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿//int n = 10;
+int[] arry = { 2, 5, 4, 7, 8, 35, 1, 3, 8, 5 };
+int i = 0;
+
+while (i < arry.Length) 
+{
+    if (arry[i] % 2 == 0)
+    {
+        Console.Write($"{arry[i]} ");
+    }
+    i = i + 1;
+}
