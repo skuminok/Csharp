9b0538579dfaefb3075d1b4ad37d5a41daf0a948
skuminok <154214571+skuminok@users.noreply.github.com>
2024-01-10 03:14:22 +0500

    add Lesson_2:Task_2

---
+++ Lesson_2/Task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+while (i < n)
+{
+    arr[i] = i + 1;
+    //Cosole.Write(arr[i]);
+    //Cosole.Write(' ');
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
+}
