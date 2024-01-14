545037a8366ab64f4d64383a6067e46f8ae328ba
skuminok <154214571+skuminok@users.noreply.github.com>
2024-01-14 21:56:39 +0500

    add Lesson3:Task3_1

---
+++ Lesson3/Task3_1/Program.cs
@@ -0,0 +1,9 @@
+﻿double CalkulateFormula(int a, int b, int c, int d)
+{
+    double numinater = a * b;
+    int denominator = c + d;
+    double result = numinater / denominator;
+    return result;
+}
+double result = CalkulateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
